92d2dd253cfe89038154e326ddf637447649ed50
Sealdhom14 <s.saldom.jobert@cmu.edu.ph>
2025-12-05 18:51:15 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,18 @@
-<script>
-// Firebase config
+// firebase-config.js
+// <-- REPLACE the values below with your Firebase project's config -->
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
+  apiKey: "AIzaSyCV5PSuNQ_dyQmXst5-Fsx4OoZu3XdcXNk",
   authDomain: "lostfoundweb-9c840.firebaseapp.com",
   projectId: "lostfoundweb-9c840",
   storageBucket: "lostfoundweb-9c840.appspot.com",
   messagingSenderId: "55877475800",
-  appId: "1:55877475800:web:80a3f984104fd495c5e6ad"
+  appId: "1:55877475800:web:247e5c96009a1ea9c5e6ad"
 };
 
-// Initialize
+// Initialize Firebase (compat)
 firebase.initializeApp(firebaseConfig);
 
-// GLOBAL Firebase variables
-const auth = firebase.auth();
-const db   = firebase.firestore();
-</script>
-
-
-
+// Make global references that other scripts use
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+window.storage = firebase.storage();
