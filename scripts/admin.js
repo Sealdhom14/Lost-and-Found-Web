e2a9cb1099c7382b908ed34d6f6b0d4e32f7edf1
Sealdhom14 <s.saldom.jobert@cmu.edu.ph>
2025-12-05 21:45:52 +0800

    Update admin.js

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,4 +1,3 @@
-// scripts/admin.js
 import {
   auth,
   db,
@@ -15,7 +14,7 @@ import {
   where,
   orderBy,
   onSnapshot
-} from "./firebase-config.js";
+} from "../firebase-config.js";
 
 // ---------------- AUTH -----------------
 
