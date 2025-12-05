1258ae8e87e96f9d8f61f620d8b4bdee9c6cfb66
Sealdhom14 <s.saldom.jobert@cmu.edu.ph>
2025-12-05 13:35:23 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCV5PSuNQ_dyQmXst5-Fsx4OoZu3XdcXNk",
+  authDomain: "lostfoundweb-9c840.firebaseapp.com",
+  projectId: "lostfoundweb-9c840",
+  storageBucket: "lostfoundweb-9c840.appspot.com",
+  messagingSenderId: "55877475800",
+  appId: "1:55877475800:web:80a3f984104fd495c5e6ad"
 };
 
 const app = initializeApp(firebaseConfig);
