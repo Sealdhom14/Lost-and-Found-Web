b405d5ee69ac9cae17348e0cd58caa5e684be3bf
Sealdhom14 <s.saldom.jobert@cmu.edu.ph>
2025-12-05 12:52:11 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
-// --- Firebase Config ---
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCV5PSuNQ_dyQmXst5-Fsx4OoZu3XdcXNk",
-    authDomain: "lostfoundweb-9c840.firebaseapp.com",
-    projectId: "lostfoundweb-9c840",
-    storageBucket: "lostfoundweb-9c840.appspot.com",
-    messagingSenderId: "55877475800",
-    appId: "1:55877475800:web:80a3f984104fd495c5e6ad"
+  apiKey: "AIzaSyCV5PSuNQ_dyQmXst5-Fsx4OoZu3XdcXNk",
+  authDomain: "lostfoundweb-9c840.firebaseapp.com",
+  projectId: "lostfoundweb-9c840",
+  storageBucket: "lostfoundweb-9c840.appspot.com",
+  messagingSenderId: "55877475800",
+  appId: "1:55877475800:web:80a3f984104fd495c5e6ad"
 };
 
-// Initialize Firebase (Compat)
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Export global variables
+// Expose to global
 window.auth = firebase.auth();
 window.db = firebase.firestore();
-window.storage = firebase.storage();
