9d8881a0c8e1b3ad1fc549669b49b0d3bd41dccb
Sealdhom14 <s.saldom.jobert@cmu.edu.ph>
2025-12-05 11:47:54 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+<script>
+  // ======= PASTE YOUR FIREBASE CONFIG HERE =======
+  // Get these values from your Firebase Console -> Project Settings -> SDK
+  const firebaseConfig = {
   apiKey: "AIzaSyCV5PSuNQ_dyQmXst5-Fsx4OoZu3XdcXNk",
   authDomain: "lostfoundweb-9c840.firebaseapp.com",
   projectId: "lostfoundweb-9c840",
-  storageBucket: "lostfoundweb-9c840.firebasestorage.app",
+  storageBucket: "lostfoundweb-9c840.appspot.com",
   messagingSenderId: "55877475800",
   appId: "1:55877475800:web:247e5c96009a1ea9c5e6ad"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase (compat)
+  firebase.initializeApp(firebaseConfig);
 
-// Firebase services
-const auth = firebase.auth();
-const db = firebase.firestore();
+  // Make globals that the rest of the app uses
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+  const storage = firebase.storage();
+</script>
